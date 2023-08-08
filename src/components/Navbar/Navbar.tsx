3aa5ede3d83de1afd25a52344aca90ed19bb0d9c
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-08-08 11:49:38 +0100

    created navbar and footer

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+// image
+import logo from '@/assets/logo-transparent-svg.svg'
+import Link from "next/link";
+
+
+export default function Navbar() {
+    return (
+        <header className="bg-pry-black px-[5%] py-4">
+            <div className="flex-between">
+                <div className="flex-center">
+                    <Image src={logo} alt="app logo" height={48} width={72} />
+                </div>
+                {/* DESKTOP NAV */}
+                <nav className="flex-center">
+                    <ul className="flex-between space-x-4 text-pry-white">
+                        <li className="nav-text"><Link href={''}>tournament</Link></li>
+                        <li className="nav-text"><Link href={''}>rules</Link></li>
+                        <li className="nav-text"><Link href={''}>partner</Link></li>
+                        <li className="nav-text"><Link href={''}>teams</Link></li>
+                        <li className="nav-text"><Link href={''}>my teams</Link></li>
+                    </ul>
+                </nav>
+                <div className="flex-center gap-4">
+                    <button type='button' className="btn white-outline">
+                        <Link href={'/'}>
+                            Login
+                        </Link>
+                    </button>
+                    <button type='button' className="btn white-btn">
+                        <Link href={'/'}>
+                            Register
+                        </Link>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
